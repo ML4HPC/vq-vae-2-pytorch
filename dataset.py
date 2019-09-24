c12912b7576492ef64cbb9fb5421b27255d22bf2
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2019-09-24 13:55:46 -0400

    Upload codes for processing datasets

--- dataset.py
+++ dataset.py
@@ -1,51 +1,97 @@
-import os
-import pickle
+from pathlib import Path
 from collections import namedtuple
 
-import torch
+import numpy as np
+import pandas as pd
+import nibabel as nib
 from torch.utils.data import Dataset
-from torchvision import datasets
-import lmdb
+
+
+__all__ = ['ABCDMidDataset', 'ABCDFrameDataset']
 
 
 CodeRow = namedtuple('CodeRow', ['top', 'bottom', 'filename'])
 
 
-class ImageFileDataset(datasets.ImageFolder):
-    def __getitem__(self, index):
-        sample, target = super().__getitem__(index)
-        path, _ = self.samples[index]
-        dirs, filename = os.path.split(path)
-        _, class_name = os.path.split(dirs)
-        filename = os.path.join(class_name, filename)
+def generate_filename_dataset(root_dir):
+    df = pd.DataFrame(None, columns=['subject', 'file_mid'])
+    subj_dirs = [p for p in Path(root_dir).glob('sub-*') if p.is_dir()]
 
-        return sample, target, filename
+    for subj_dir in subj_dirs:
+        fns_mid = sorted(subj_dir.glob('ses-*/func/*task-mid*bold.nii*'))
+
+        for fn_mid in fns_mid:
+            df = df.append(pd.Series({
+                'subject': subj_dir.name.split('-')[1],
+                'file_mid': str(fn_mid)
+            }), ignore_index=True)
+
+    return df
 
 
-class LMDBDataset(Dataset):
-    def __init__(self, path):
-        self.env = lmdb.open(
-            path,
-            max_readers=32,
-            readonly=True,
-            lock=False,
-            readahead=False,
-            meminit=False,
-        )
+def generate_subject_dataset(root_dir):
+    subj_dirs = [p for p in root_dir.glob('sub-*') if p.is_dir()]
+    subjects = [p.name.split('-')[1] for p in subj_dirs]
+    return pd.DataFrame({'subject': subjects})
 
-        if not self.env:
-            raise IOError('Cannot open lmdb dataset', path)
 
-        with self.env.begin(write=False) as txn:
-            self.length = int(txn.get('length'.encode('utf-8')).decode('utf-8'))
+def load_nifti_to_numpy(filename):
+    img = nib.load(str(filename))
+    return np.array(img.dataobj)
+
+
+class ABCDMidDataset(Dataset):
+    def __init__(self, root_dir, subject_wise=True):
+        self.root_dir = root_dir
+        self.subject_wise = subject_wise
+        if subject_wise:
+            self.df = generate_subject_dataset(root_dir)
+        else:
+            self.df = generate_filename_dataset(root_dir)
 
     def __len__(self):
-        return self.length
+        return len(self.df)
 
-    def __getitem__(self, index):
-        with self.env.begin(write=False) as txn:
-            key = str(index).encode('utf-8')
+    def _getitem_filename(self, idx):
+        row = self.df.iloc[idx]
 
-            row = pickle.loads(txn.get(key))
+        subject = row['subject']
+        file_mid = row['file_mid']
 
-        return torch.from_numpy(row.top), torch.from_numpy(row.bottom), row.filename
+        arr_mid = load_nifti_to_numpy(file_mid)
+
+        return {'subject': subject, 'file_mid': file_mid, 'mid': arr_mid}
+
+    def _getitem_subject(self, idx):
+        row = self.df.iloc[idx]
+        subject = row['subject']
+
+        subj_dir = Path(self.root_dir) / ('sub-' + subject)
+        file_mid = np.random.choice(
+            [*subj_dir.glob('ses-*/func/*task-mid*bold.nii*')])
+
+        arr_mid = load_nifti_to_numpy(file_mid)
+
+        return {'subject': subject, 'file_mid': file_mid, 'mid': arr_mid}
+
+    def __getitem__(self, idx):
+        if self.subject_wise:
+            return self._getitem_subject(idx)
+        else:
+            return self._getitem_filename(idx)
+
+
+class ABCDFrameDataset(Dataset):
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.list_files = sorted(Path(root_dir).glob('*.npy'))
+
+    def __len__(self):
+        return len(self.list_files)
+
+    def __getitem__(self, idx):
+        filename = self.list_files[idx]
+        arr = np.load(filename)
+        arr = np.pad(arr, [[0, 0], [0, 0], [2, 2]], mode='edge').shape
+        arr = np.expand_dims(arr, axis=0)
+        return arr
---
+++ extract_frame.py
@@ -0,0 +1,34 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+
+from dataset import ABCDMidDataset
+
+
+def extract_frames(in_path, out_path):
+    in_path = Path(in_path)
+    out_path = Path(out_path)
+
+    dataset = ABCDMidDataset(in_path)
+
+    if not out_path.exists():
+        out_path.mkdir()
+
+    for row in dataset:
+        print(row['subject'])
+        arr = np.moveaxis(row['mid'], -1, 0)
+        for i in range(len(arr)):
+            np.save(out_path / Path(row['file_mid']).name.replace(
+                '_bold.nii', f'_frame-{i:03d}.npy'), arr[i])
+        del row, arr
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('in_path', type=str)
+    parser.add_argument('out_path',  type=str)
+
+    args = parser.parse_args()
+
+    extract_frames(args.in_path, args.out_path)
